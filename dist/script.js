6f2b799b4b61dc46301df5023470409fa46dd93e
ArtyomNajaryan <najaryan97@mail.ru>
2022-03-01 18:48:10 +0400

    mobile

--- dist/script.js
+++ dist/script.js
@@ -14,23 +14,36 @@ function wmportfolio() {
 
   }
 
+/*** Language menu toggle*/
+var langmenu = document.getElementById("LangMenuToggle");
+var mainmenu = document.getElementById("LangMenuToggle");
+
+//langmenu.addEventListener("click", togglemenu('LangMenu'));
+
+document.getElementById("LangMenuToggle").onclick = function() {togglemenu('LangMenu')};
+document.getElementById("menuToggle").onclick = function() {togglemenu('mobile-menu')};
+
+
+
+function togglemenu(elmnt) {
+  var element = document.getElementById(elmnt);
+  element.classList.toggle("hidden");
+
+if (elmnt == "mobile-menu") {
+  element.classList.toggle("opacity-50");
+  element.classList.toggle("top-20");
+}
+}
+
 
   
-  document.getElementsById('LangMenuToggle').addEventListener('click',wmLanguage)
-  function wmLanguage(){
+  document.body.addEventListener ('click',wmLanguages)
+  function wmLanguages(){
     let x = document.getElementById("LangMenu");
-    if (x.style.display === "none") {
-      x.style.display = "inline";
-    } else {
-      x.style.display = "none";
-    }
+    let y = document.getElementById("LangMenuToggle");
+
+    if (x.style.display == y.style.display) {
+      x.style.display = "hidden";
+    } 
   }
  
-  function mobileMenu(){
-    let x = document.getElementById("mobile-menu");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
